b0bbc6a5f0f65a41c0fecaf5df2335e74316f57f
Igor Gramkow <gramkowigor@gmail.com>
2024-08-21 07:30:08 -0300

    creating some json and writter tests

--- src/controllers/controllers.go
+++ src/controllers/controllers.go
@@ -1,9 +1,25 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+// Utilizando o writer do FMT
+func FmtWriter(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Utilizando o writter do fmt!")
+}
+
+// Exemplos de JSON
+func JsonEncode(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Resposta por encoder!")
+}
+
+func JsonDirectlyWrited(w http.ResponseWriter, r *http.Request) {
+	mensagem, _ := json.Marshal("Resposta escrita diretamente!")
+	w.Write(mensagem)
+}
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Home Called!")
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -8,5 +8,8 @@ import (
 
 func HandleRequest() {
 	http.HandleFunc("/", controllers.Home)
+	http.HandleFunc("/fmt", controllers.FmtWriter)
+	http.HandleFunc("/json-direct", controllers.JsonDirectlyWrited)
+	http.HandleFunc("/json-encoder", controllers.JsonEncode)
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
